977e3e66f4f0ccc286e8eb3064ec2d3154507760
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-02-25 22:41:12 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  const [food, setFood] = useState([])
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+    .then(res => res.json())
+    .then(data => setFood(data.meals))
+  },[])
+  return(
+    <>
+    {
+      console.log(food)
+    }
+    <div className="container">
+      <h2 className="text-center mt-5">Search a meal what do you want</h2>
+      <div className="d-flex mt-5 mx-auto px-3" style={{maxWidth:'40rem'}}>
+      <input className="form-control" type="text" name="" placeholder="Search by first letter of food"/>
+      <button  className="btn btn-primary">Search</button>
+      </div>
     </div>
-  );
+    </>
+   
+  )
 }
 
-export default App;
+
+export default App
